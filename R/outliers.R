4a0d6216af0836f9488754d0509a2839c711aa5d
dnacombo <maximilien.chaumon@gmail.com>
2016-07-01 16:40:31 +0200

    First commit

--- R/outliers.R
+++ R/outliers.R
@@ -7,16 +7,17 @@
 #' @param tail One of \code{'both'}, \code{'left'}, or \code{'right'}
 #' @param meth One of \code{'trim'} or \code{'IQR'}
 #' @param probs A 1 or 2 element numeric vector of probabilities
+#' @param sdn when meth = 'sd', number of standard deviations away from the mean to detect outlier
 #' @param na.rm A boolean whether to remove NA
-#' @return A cleaned vector where all outliers outside of \code{quantile(x,probs)} have been removed.
+#' @return A cleaned vector where all outliers outside of \code{quantile(x,probs)} (see details for individual methods) are changed to NA.
+#' @details Each method will be described here in the future.
 #' @examples
-#' x <- randn(1000)
+#' x <- rnorm(1000)
 #' y <- rm.outliers(x,probs=c(.01,.99))
 #' 
 
-rm.outliers <- function(x, tail='both', meth='trim', probs=c(.25, .75), na.rm = TRUE, ...) {
-  y[find.outliers(x, tail=tail, meth=meth, probs=probs, na.rm=na.rm)] <- NA
-  y
+rm.outliers <- function(x, tail='both', meth='trim', probs=c(.25, .75), sd=2, na.rm = TRUE, ...) {
+  x[find.outliers(x, tail=tail, meth=meth, probs=probs, na.rm=na.rm)] <- NA
 }
 
 
@@ -25,27 +26,38 @@ rm.outliers <- function(x, tail='both', meth='trim', probs=c(.25, .75), na.rm = 
 #' Return logical true where outliers are in a vector.
 #' 
 #' @param x A vector
+#' @param tail One of \code{'both'}, \code{'left'}, or \code{'right'}
+#' @param meth One of \code{'trim'}, \code{'sd'} or \code{'IQR'}
 #' @param probs A 1 or 2 element numeric vector of probabilities
+#' @param sdn when meth = 'sd', number of standard deviations away from the mean to detect outlier
 #' @param na.rm A boolean whether to remove NA
-#' @return A cleaned vector where all outliers outside of \code{quantile(x,probs)} have been removed.
+#' @return A boolean vector where outliers outside of \code{quantile(x,probs)} (see details for individual methods) are true.
+#' @details Each method will be described here in the future.
 #' @examples
-#' x <- randn(1000)
+#' x <- rnorm(1000)
 #' y <- find.outliers(x,probs=c(.01,.99))
 #' 
 
-find.outliers <- function(x, tail='both', meth='trim', probs=c(.25, .75), na.rm = TRUE, ...) {
+find.outliers <- function(x, tail='both', meth='trim', probs=c(.25, .75), sdn=2, na.rm = TRUE, ...) {
   if (length(probs) == 1) {
     probs = c(probs, 1-probs)
   }
-  
-  if (meth == 'IQR'){
-    qnt <- quantile(x, probs=probs, na.rm = na.rm, ...)
-    H <- 1.5 * IQR(x, na.rm = na.rm)
-  }
-  if (meth == 'trim'){
-    qnt <- quantile(x, probs=probs, na.rm = na.rm, ...)
-    H <- 0
-  }
+  switch(meth,
+         IQR={
+           qnt <- quantile(x, probs=probs, na.rm = na.rm, ...)
+           H <- 1.5 * IQR(x, na.rm = na.rm)
+         },
+         trim={
+           qnt <- quantile(x, probs=probs, na.rm = na.rm, ...)
+           H <- 0
+         },
+         sd={
+           s = sd(x,na.rm = na.rm)
+           m = mean(x, na.rm = na.rm)
+           qnt <- c(m - sdn * s , m + sdn * s)
+           H <- 0
+         })
+         
   y <- logical(length=length(x))
   ifelse(match(tail,c('both','left')),y[x < (qnt[1] - H)] <- T)
   ifelse(match(tail,c('both','right')),y[x > (qnt[2] + H)] <- T)
