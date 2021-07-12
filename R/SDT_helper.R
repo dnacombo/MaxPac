5c15860e534465654892ce42dab0c61566409e2a
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2021-07-12 17:24:23 +0200

    add formula reference to Stanislaw and Todorov 1999 add B, Aprime, B"

--- R/SDT_helper.R
+++ R/SDT_helper.R
@@ -2,6 +2,7 @@
 #' YN dprime estimator
 #' 
 #' Estimate dprime from Hit Rate and False Alarm Rate in a Yes/No task
+#' Formula (1) in Stanislaw and Todorov 1999
 #' 
 #' @param hitR Hit Rate
 #' @param faR False Alarm Rate
@@ -12,9 +13,52 @@ perf2dp <- function(hitR,faR) {
   qnorm(hitR) - qnorm(faR)
 }
 
+#' YN Aprime estimator
+#' 
+#' Estimate Aprime from Hit Rate and False Alarm Rate in a Yes/No task
+#' Formula (3) in Stanislaw and Todorov 1999
+#' 
+#' @param hitR Hit Rate
+#' @param faR False Alarm Rate
+#' @return Aprime value
+#' 
+
+perf2Ap <- function(hitR,faR) {
+  .5 + (sign(hitR - faR) * ((hitR - faR)^2 + abs(hitR - faR)) / (4 * max(hitR, faR) - 4 * hitR * faR))
+}
+
+#' YN B" estimator
+#' 
+#' Estimate B" from Hit Rate and False Alarm Rate in a Yes/No task
+#' Formula (9) in Stanislaw and Todorov 1999
+#' 
+#' @param hitR Hit Rate
+#' @param faR False Alarm Rate
+#' @return B" value
+#' 
+
+perf2Bsecond <- function(hitR,faR) {
+  sign(hitR - faR) * (hitR * (1 - hitR) - faR * (1 - faR)) / (hitR * (1 - hitR) + faR * (1 - faR))
+}
+
+#' YN bias (B) estimator
+#' 
+#' Estimate bias (B) from Hit Rate and False Alarm Rate in a Yes/No task
+#' Formula (5) in Stanislaw and Todorov 1999 (note typo in formula, but not in software implementations later in pdf)
+#' 
+#' @param hitR Hit Rate
+#' @param faR False Alarm Rate
+#' @return B value
+#' 
+
+perf2B <- function(hitR,faR){
+  exp((qnorm(faR)^2 - qnorm(hitR)^2) / 2)
+}
+
 #' YN criterion (c) estimator
 #' 
 #' Estimate criterion (c) from Hit Rate and False Alarm Rate in a Yes/No task
+#' Formula (7) in Stanislaw and Todorov 1999
 #' 
 #' @param hitR Hit Rate
 #' @param faR False Alarm Rate
@@ -27,7 +71,7 @@ perf2c <- function(hitR,faR){
 
 #' YN dprime (dp) and criterion (c) estimator
 #' 
-#' Estimate criterion (c) from Hit Rate and False Alarm Rate in a Yes/No task
+#' Estimate dprime (dp) and criterion (c) from Hit Rate and False Alarm Rate in a Yes/No task
 #' 
 #' @param hitR Hit Rate
 #' @param faR False Alarm Rate
@@ -53,3 +97,4 @@ dpc2perf <- function(dp,c){
   perfs <- list(HR=HR,MR=1-HR, FAR=FAR,CR=1-FAR)
   perfs
 }
+
